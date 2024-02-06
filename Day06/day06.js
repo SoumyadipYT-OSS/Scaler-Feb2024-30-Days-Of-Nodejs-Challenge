5fa5546f8832fa4e0bb1da5591542db3740bd089
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-02-06 23:39:00 +0530

    Updated the Day06 challenge Node JS

--- Day06/day06.js
+++ Day06/day06.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const app = express();
-
 /**
  * Handles GET requests to "/greet" endpoint
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  */
 
+const express = require('express')
+const app = express()
+
+app.get("/greet", greetHandler);
+
+const port=process.env.PORT||5500
+
+app.listen(port, () => {
+    console.log(`port is running in ${port}`);
+});
+
 function greetHandler(req, res) {
     const name = req.query.name;
-
-    if (name) {
-        res.send(`Hello, ${name}!`);
-    } else {
-        res.send('Hello, Guest!');
-    }
+    const greeting = name ? `Hello,  ${name}`:`Hello, guest!`;  // used ternary operator
+    res.send(greeting);
 }
-
-
-app.get('/greet', greetHandler);
-const port = 5500;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
