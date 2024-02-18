b8f50f6200b79edee9f7215ced8f461da3f94087
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-02-18 17:21:31 +0530

    Updated upto Day18

--- Day18/day18.js
+++ Day18/day18.js
@@ -1,34 +1,50 @@
-const mng = require('mongoose')
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Define User schema and create User model
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+});
+const User = mongoose.model('User', UserSchema);
+
+mongoose.connect('mongodb://localhost:27017/day18', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const app = express();
+app.get('/users', getAllUsers);
 
 function getAllUsers(req, res) {
-    userModel.find().then((users) => {
-        console.log("User database is fetched...");
-        res.json(users);
-    }).catch((error) => {
-        console.log("Error retrieving users:", error.message);
+  User.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      console.error(`Error retrieving users: ${error.message}`);
+      res.status(500).send(`Error retrieving users: ${error.message}`);
     });
 }
 
+const PORT = process.env.PORT || 5400;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-const userSchema = new mng.Schema({
-    username: { type: String, required: true},
-    email: { type: String, required: true, unique: true }
-});
 
-const userModel = mng.model("User", userSchema)
+// Add a new user
+async function addUser(user) {
+    const newUser = new User({
+        username: user.username,
+        email: user.email
+    });
 
-mng.connect('mongodb://localhost:27018/day18').then(() => {
-    console.log('Connected to MongoDB!');
-}).catch((err) => {
-    console.log('Error:', err);
-});
-
-app.get('/users', getAllUsers)
-
-const PORT = 5400;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    try {
+        const res = await newUser.save();
+        console.log("New user is created");
+    } catch (error) {
+        console.log("Error creating user: ", error.message);
+    }
+}
+addUser({
+    username: "Dishant Yadav",
+    email: "dishant-test@gmail.com",
 });
